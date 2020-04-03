48d4476994351d74e8c5a89f0bb8e7b577aa1582
Leo Maroni <me@leomaroni.de>
2020-04-03 02:12:18 +0200

    (WIP) Refactoring web to differnt roles; Docker to standalone
    * Extracted some Docker services to standalone
    * Added new hosts
    * ...

--- roles/web/services/roles/labcode.de/matrix.labcode.de/files/backup.sh
+++
@@ -1,7 +0,0 @@
-docker stop matrix.labcode.de-app
-
-docker exec matrix.labcode.de-db pg_dumpall -c -U synapse > /volume/backup/labcode.de/matrix.labcode.de/db_data.sql
-
-chown rsync-backup:rsync-backup /volume/backup/labcode.de/matrix.labcode.de/*
-
-docker start matrix.labcode.de-app
